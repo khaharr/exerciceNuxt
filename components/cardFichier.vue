c873a1ab6fd766ad7ad5bfa10538193514853489
khalid <kharrari-mdaloc@M092-1630107.ad.infra.dgfip>
2024-04-26 09:40:30 +0200

    api fetch en fichier local et pas en https (cardFichier.vue , (api: info.ts)) index.vue

--- components/cardFichier.vue
+++ components/cardFichier.vue
@@ -1,29 +1,27 @@
 <template>
   <div class="animationCard maxTaille">
-    <!-- ce code sert a afficher une card avec images texte et l input de fichier  -->
-    <div class=" d-flex align-items-start grid gap-5 ">
-      <div class="card  " style="max-width: 540px">
+    <!-- ce code sert a afficher une card avec images texte et l input de fichier -->
+    <div class="d-flex align-items-start grid gap-5">
+      <div class="card" style="max-width: 540px">
         <div class="row g-0">
-          <div class="col-md-4">
-            <img src="assets\img\login.webp" class="img-fluid rounded-start" alt="..." />
-          </div>
+          <div class="col-md-4"><img :src="image" class="img-fluid rounded-start" alt="..." /></div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title"> {{ titre }}</h5>
+              <h5 class="card-title">{{ title }}</h5>
               <!--condition longueur text -->
-              <p v-if="name.length < 30 " class="card-text text-danger">
+              <p v-if="description.length < 30" class="card-text text-danger">
                 <!-- sert a afficher le texte present dans la variable name -->
                 Veuillez insérer plus de 30 caractères !
               </p>
-              <p v-else> {{ name }}</p>
-
+              <p v-else>{{ description }}</p>
+              <p> categorie : {{ category }}</p>
               <p class="card-text">
-                <small class="text-body-secondary">il y a {{ id }} min </small>
+                <small class="text-body-secondary">le prix est a {{ price }} €</small>
               </p>
             </div>
           </div>
         </div>
-            <div class="input-group">
+        <div class="input-group">
           <input
             type="file"
             class="form-control"
@@ -31,39 +29,37 @@
             aria-describedby="inputGroupFileAddon04"
             aria-label="Upload"
           />
-          <button class="btn btn-outline-secondary" @click="myTest" type="button" id="inputGroupFileAddon04">Valider</button>
+          <button class="btn btn-outline-secondary" @click="myTest" type="button" id="inputGroupFileAddon04">
+            Valider
+          </button>
         </div>
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
 export default {
   name: "cardFichier",
-  props: ["id", "name","titre"],
-  methods:{
-    myTest: function() {
-        alert("valide avec succés")
-    }
-}
+  props: ["id", "title", "price", "description", "category", "image", "name"],
+  methods: {
+    myTest() {
+      alert("valide avec succés");
+    },
+  },
 };
-
-
 </script>
 
 <style lang="css" scoped>
-.animationCard{
-    transition: transform .3s ease-out;
+.animationCard {
+  transition: transform 0.3s ease-out;
   display: inline-block;
 }
-.animationCard:hover{
-    transform: translateY(-10px);
+.animationCard:hover {
+  transform: translateY(-10px);
   box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.35);
-
-}   
-.maxTaille{
-    margin-top:50px;
+}
+.maxTaille {
+  margin-top: 50px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,24 @@
 <template>
-    <!-- <div class="d-flex large px-5 cool">   -->
-        <div class="d-flex  justify-content-between flex-wrap  cool">
- 
-    <cardFichier v-for="elt in elts" :id="elt.id" :name="elt.name" :titre="elt.titre"/>
-</div>
-<!-- </div> -->
+  <div class="d-flex flex-wrap cool">
+    <div class="col-4" v-for="elt in elts" :key="elt.id">
+      <cardFichier
+        :id="elt.id"
+        :title="elt.title"
+        :price="elt.price"
+        :description="elt.description"
+        :category="elt.category"
+        :image="elt.image"
+        :name="elt.name"
+      />
+    </div>
+  </div>
 </template>
-
-
 <script setup>
-
-const elts  =  await $fetch("/api/infoCard")
-
+const elts = await $fetch("/api/info");
 </script>
-
 <style lang="scss" scoped>
-.cool{
-    background-color: rgb(241, 241, 241);
+.cool {
+  background-color: rgb(241, 241, 241);
 }
+
 </style>
